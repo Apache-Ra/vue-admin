a946f74cde00e1b6c9dc0b91da46b88724552f3d
Apache-Ra <summer.vip.20@qq.com>
2020-07-06 10:24:31 +0800

    config update 1.0.0

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,22 @@ import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+console.log(process.env.NODE_ENV);
 
+let now = new Date();
+let year = now.getFullYear();
+let month = now.getMonth();
+let date = now.getDate();
+
+let month_, date_;
+if (month < 10) {
+  month_ = '0' + month;
+}
+if (date < 10) {
+  date_ = '0' + date;
+}
+
+console.log(year + '-' + month_ + '-' + date_);
 new Vue({
   router,
   store,
